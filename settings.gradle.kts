d3ec36d70371f23f68aaa9406a6294e220d2fac5
trainb0y <trainb0ydev@gmail.com>
2023-01-02 17:06:33 -0700

    Basic Quilt mod with Kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "Hydrazine"
+
+pluginManagement {
+	repositories {
+		maven("https://maven.quiltmc.org/repository/release") {
+			name = "Quilt"
+		}
+		// Currently needed for Intermediary and other temporary dependencies
+		maven("https://maven.fabricmc.net/") {
+			name = "Fabric"
+		}
+		gradlePluginPortal()
+	}
+}
---
+++ src/main/kotlin/io/github/hydrazinemc/hzmod/server/Server.kt
@@ -0,0 +1,10 @@
+package io.github.hydrazinemc.hzmod.server
+
+import org.quiltmc.loader.api.ModContainer
+import org.quiltmc.qsl.base.api.entrypoint.server.DedicatedServerModInitializer
+
+class Server : DedicatedServerModInitializer {
+	override fun onInitializeServer(mod: ModContainer?) {
+		println("Hello from the server!")
+	}
+}

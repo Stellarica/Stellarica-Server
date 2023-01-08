c5b5960db4ece3ce65424bef9d8005506c8f41c1
trainb0y <trainb0ydev@gmail.com>
2023-01-07 19:03:15 -0700

    BlockEntityMixin

---
+++ src/main/java/io/github/hydrazinemc/hzmod/mixin/BlockEntityMixin.java
@@ -0,0 +1,14 @@
+package io.github.hydrazinemc.hzmod.mixin;
+
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(BlockEntity.class)
+public interface BlockEntityMixin {
+	@Mutable
+	@Accessor("pos")
+	void setPos(BlockPos pos);
+}

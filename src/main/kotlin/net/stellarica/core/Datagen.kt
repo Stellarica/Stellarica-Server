2143ebbc922286fee780b0884be0fcc29d73651d
trainb0y <trainb0ydev@gmail.com>
2023-01-29 08:34:40 -0700

    Set up datagen

---
+++ src/main/kotlin/net/stellarica/core/Datagen.kt
@@ -0,0 +1,10 @@
+package net.stellarica.core
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+class Datagen: DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator?) {
+
+	}
+}

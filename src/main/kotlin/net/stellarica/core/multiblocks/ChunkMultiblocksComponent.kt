b2f79c8023e0b7fa141d0c818487140827646959
trainb0y <trainb0ydev@gmail.com>
2023-02-03 11:50:09 -0700

    Serialize multiblocks to nbt instead of string

--- src/main/kotlin/net/stellarica/core/multiblocks/ChunkMultiblocksComponent.kt
+++ src/main/kotlin/net/stellarica/core/multiblocks/ChunkMultiblocksComponent.kt
@@ -3,15 +3,15 @@ package net.stellarica.core.multiblocks
 
 import dev.onyxstudios.cca.api.v3.component.tick.ServerTickingComponent
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.world.World
 import net.minecraft.world.chunk.Chunk
 import net.minecraft.world.chunk.WorldChunk
+import net.silkmc.silk.nbt.serialization.Nbt
+import net.silkmc.silk.nbt.serialization.decodeFromNbtElement
+import net.silkmc.silk.nbt.serialization.encodeToNbtElement
 import net.stellarica.core.Stellarica.Companion.identifier
 import net.stellarica.core.event.multiblocks.MultiblockUndetectEvent
 
@@ -49,21 +49,15 @@ class ChunkMultiblocksComponent(private val chunk: Chunk) : ServerTickingCompone
 	}
 
 	override fun readFromNbt(tag: NbtCompound) {
-		// read multiblocks from tag
-		// todo: don't use a string in the nbt, better to just serialize to nbt
-		if (tag.getString("multiblocks") == "") return
 		val world = (chunk as WorldChunk).world
-		multiblocks.addAll(
-			Json.decodeFromString<Set<MultiblockData>>(tag.getString("multiblocks")).map { it.toInstance(world) }
-				.toMutableSet()
-		)
+		tag.get("multiblocks")?.let { nbt ->
+			Nbt.decodeFromNbtElement<List<MultiblockData>>(nbt).map { it.toInstance(world) }
+		}?.let { multiblocks.addAll(it) }
 	}
 
 	override fun writeToNbt(tag: NbtCompound) {
-		// serialize multiblocks to tag
-		// todo: see above todo
 		if (multiblocks.size == 0) return
-		tag.putString("multiblocks", Json.encodeToString(multiblocks.map { MultiblockData(it) }))
+		tag.put("multiblocks", Nbt.encodeToNbtElement(multiblocks.map { MultiblockData(it) }))
 	}
 
 	override fun serverTick() {
